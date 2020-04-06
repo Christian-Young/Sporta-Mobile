43db828128efdb3cf22c6ff8e867a99acb4277a9
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-05 20:51:35 -0400

    placeholder textfield for fields retrieved from the DB

--- Profile.dart
+++ Profile.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
           title: Text('Profile')
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: TextField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'First Name',
+                  )
+                )
+              ),
+            ]
+        )
+      )
     );
   }
 }
