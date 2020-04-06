72356cce226f0c0e4261df28c76ebb8e74425f06
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-05 20:50:56 -0400

    nothing

--- main.dart
+++ main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'Homepage.dart';
 import 'Login.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Sporta());
 
 // Root
-class MyApp extends StatelessWidget {
+class Sporta extends StatelessWidget {
 
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Affects the colors of the app.
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
