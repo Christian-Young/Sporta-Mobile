25fa69295226dd6258891f90c26893e80f69e6a5
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-05 20:49:58 -0400

    added 3 tabs, overview, sports, and more sports

--- Homepage.dart
+++ Homepage.dart
@@ -3,57 +3,84 @@ import 'Settings.dart';
 import 'Profile.dart';
 
 class Homepage extends StatelessWidget{
+
+  void welcome(BuildContext context){
+    final snackBar = SnackBar(content: Text("Welcome [name here]!"));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home')
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+      home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+              bottom: TabBar(
+                  tabs: [
+                    // Overview
+                    Tab(icon: Icon(Icons.library_books)),
+                    // Sports
+                    Tab(icon: Icon(Icons.directions_run)),
+                    // More sports
+                    Tab(icon: Icon(Icons.access_alarm))
+                  ]
               ),
-              child: Text(
-                'Options',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
             ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-              onTap: (){
-                Navigator.push(
-                    context, MaterialPageRoute(
-                    builder: (context) => Profile()));
-              }
+            body: TabBarView(
+                children: [
+                Text("Overview"),
+                Text("Sports"),
+                Text("More sports")
+              ]
             ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.push(
-                    context, MaterialPageRoute(
-                    builder: (context) => Settings()));
-              }
+            drawer: Drawer(
+                child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      DrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Colors.deepOrange,
+                        ),
+                        child: Text('Options',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                          leading: Icon(Icons.account_circle),
+                          title: Text('Profile'),
+                          onTap: (){
+                            Navigator.push(
+                                context, MaterialPageRoute(
+                                builder: (context) => Profile()));
+                          }
+                          ),
+                      ListTile(
+                          leading: Icon(Icons.settings),
+                          title: Text('Settings'),
+                          onTap: () {
+                            Navigator.push(
+                                context, MaterialPageRoute(
+                                builder: (context) => Settings()));
+                          }
+                          ),
+                      ListTile(
+                        leading: Icon(Icons.arrow_back),
+                        title: Text('Logout'),
+                        onTap: () {
+                          Navigator.pop(context);
+                          },
+                      )
+                    ]
+                )
             ),
-            ListTile(
-              leading: Icon(Icons.arrow_back),
-              title: Text('Logout'),
-              onTap: () {
-                // Close the drawer
-                Navigator.pop(context);
-                // Logout
-                Navigator.pop(context);
-              },
-            )
-          ]
-        )
+          )
       )
     );
   }
