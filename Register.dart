06694447659eb8f9737fa838e1e7e7b94ad18e09
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-09 21:31:26 -0400

    added post

--- Register.dart
+++ Register.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/RegInfo.dart';
+import 'dart:async';
+import 'dart:convert';
 
 class Register extends StatelessWidget{
   final _firstNameController = TextEditingController();
@@ -25,10 +29,32 @@ class Register extends StatelessWidget{
         }
     );
   }
-  // TODO: If all credentials are valid, put them in the db and assure the user they are now registered.
-  bool verifyRegistration(String firstName, String lastName, String email, String password, BuildContext context){
 
-    if (firstName.length == 0 || lastName.length == 0 || email.length == 0 || password.length == 0){
+  Future<RegInfo> reg(String firstname, String lastname, String email, String password) async {
+    final http.Response response = await http.post(
+      'http://localhost:8081/api/users/register',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'firstname': firstname,
+        'lastname': lastname,
+        'email': email,
+        'password': password,
+      }),
+    );
+    if (response.statusCode == 200) {
+      print("Registration successful");
+      return RegInfo.fromJson(json.decode(response.body));
+    } else {
+
+      throw Exception('Failed');
+    }
+  }
+
+  bool verifyRegistration(String firstname, String lastname, String email, String password, BuildContext context){
+
+    if (firstname.length == 0 || lastname.length == 0 || email.length == 0 || password.length == 0){
       _showDialog(context, "Please fill in all fields.");
       return false;
     }
@@ -36,6 +62,9 @@ class Register extends StatelessWidget{
       _showDialog(context, "Password too short.");
       return false;
     }
+
+    // Register
+    //reg(firstname, lastname, email, password);
 
     return true;
   }
@@ -91,7 +120,7 @@ class Register extends StatelessWidget{
                       _emailController.text, _passwordController.text, context)) {
                     Navigator.pop(context);
                     _showDialog(context, "Account created!");
-                  };
+                  }
                 },
                 child: Text('Register')
             ),
