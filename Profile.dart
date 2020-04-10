f52ad653cfdb6a300e2f0838faa7d7df36ed25cb
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-09 21:31:16 -0400

    set up editable fields

--- Profile.dart
+++ Profile.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
-  final _firstNameController = TextEditingController();
-  final _lastNameController = TextEditingController();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final _firstNameController = TextEditingController(text: 'first name init');
+  final _lastNameController = TextEditingController(text: 'last name init');
+  final _emailController = TextEditingController(text: 'email init');
+  final _heightController = TextEditingController(text: 'height init');
+  final _ageController = TextEditingController(text: 'age init');
 
   Widget build(BuildContext context){
     return Scaffold(
@@ -16,7 +22,7 @@ class Profile extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                 child: TextField(
                   controller: _firstNameController,
                   decoration: InputDecoration(
@@ -24,6 +30,46 @@ class Profile extends StatelessWidget {
                     labelText: 'First Name',
                   )
                 )
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextField(
+                      controller: _lastNameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Last Name',
+                      )
+                  )
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      )
+                  )
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextField(
+                      controller: _heightController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Height',
+                      )
+                  )
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextField(
+                      controller: _ageController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Age',
+                      )
+                  )
               ),
             ]
         )
