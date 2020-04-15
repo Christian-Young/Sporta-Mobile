99563056cc107c28a011bb5b71912b7e7d6d602d
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-15 00:00:38 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,7 +6,6 @@ void main() => runApp(Sporta());
 // Root
 class Sporta extends StatelessWidget {
 
-
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
