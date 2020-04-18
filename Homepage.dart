dec2ca97fcdcdaaa060698ea04aa94c8d49a2ca5
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-17 23:44:56 -0400

    Bag update.

--- Homepage.dart
+++ Homepage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'Profile.dart';
 import 'CreateEvent.dart';
+import 'ShowDialog.dart';
 import 'package:http/http.dart' as http;
 import 'models/UserInfo.dart';
+import 'models/BagInfo.dart';
+import 'models/ClubInfo.dart';
+import 'models/GetBags.dart';
 import 'Session.dart';
 import 'dart:convert';
 import 'dart:async';
@@ -11,27 +15,156 @@ class Homepage extends StatefulWidget{
   _Homepage createState() => _Homepage();
 }
 
-class _Homepage extends State<Homepage>{
+/*
+class _GolfBagDialogState extends State<GolfBagDialog>{
+  // Controllers
+  final _bagNameController = TextEditingController();
+  final _clubNameController = TextEditingController();
+  final _clubTypeController = TextEditingController();
   // Index variable for the tabs.
   int _selectedIndex = 0;
   // Bag count
-  static int _bags = 0;
+  int _bags = 0;
+  // Number of clubs
+  int _numClubs = 0;
+  // Dropdownvalue for number of bags
+  String _dropdownValue = 'Wood';
+
+  Widget build(BuildContext context){
+    showDialog(
+      context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+            'Bag', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                    controller: _bagNameController,
+                    decoration: InputDecoration(hintText: "Bag Name")
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 35, 0, 0),
+                  child: Text(
+                      "Club", style: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold)),
+                ),
+                TextField(
+                    controller: _clubNameController,
+                    decoration: InputDecoration(hintText: "Club Name")
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Text("Club type"),
+                ),
+                DropdownButton<String>(
+                  value: _dropdownValue,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.deepOrange),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      _dropdownValue = newValue;
+                    });
+                  },
+                  items: <String>['Wood', 'Iron', 'Wedge', 'Putter'].map<
+                      DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value, style: TextStyle(fontSize: 18)),
+                    );
+                  }).toList(),
+                ),
+                RaisedButton(
+                  child: Text("Add club"),
+                  onPressed: () {
+                    if (_clubNameController.text == "")
+                      DialogPopUp(context, "Name your club.");
+                    setState(() {
+                      _numClubs++;
+                    });
+                  },
+                ),
+                Text("$_numClubs clubs will be in this bag",
+                    style: TextStyle(fontSize: 18))
+              ],
+            )
+        ),
+        actions: <Widget>[
+          new FlatButton(
+              child: new Text('Add bag'),
+              onPressed: () {
+                if (_bagNameController.text == "")
+                  DialogPopUp(context, "Name your bag.");
+                else {
+                  Navigator.of(context).pop();
+                }
+              }
+          ),
+          new FlatButton(
+              child: new Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _bagNameController.text = "";
+                _clubNameController.text = "";
+                _numClubs = 0;
+              }
+          )
+        ],
+      );
+    });
+  }
+}
+
+class GolfBagDialog extends StatefulWidget{
+  @override
+  _GolfBagDialogState createState() => new _GolfBagDialogState();
+}
+*/
+
+class _Homepage extends State<Homepage>{
+  // Controllers
+  final _bagNameController = TextEditingController();
+  final _clubNameController = TextEditingController();
+  // Index variable for the tabs.
+  int _selectedIndex = 0;
+  // Bag count
+  int _bags = 0;
+  // Number of clubs
+  int _numClubs = 0;
+  // Drop down value for number of bags
+  String _clubType = 'Wood';
+  // POST Bag model
+  Future<BagInfo> futureBagInfo;
+  // Club model
+  Future<ClubInfo> futureClubInfo;
+  // Get all bags model
+  Future<GetBags> futureGetBags;
+  // User model
+  Future<UserInfo> futureProfileInfo;
+  final String NO_CLUB = "0";
+  final String WOOD = "1";
+  final String IRON = "2";
+  final String WEDGE = "3";
+  final String PUTTER = "4";
+  String bagID;
+  List<Widget> BagList = new List<Widget>();
 
   // Large font style.
   static const TextStyle largeStyle = TextStyle(
       fontSize: 30,
       fontWeight: FontWeight.bold
   );
-
   // Initial state. Get the user and details.
   @override
   void initState()
   {
     super.initState();
     // Get profile info to display it on the profile page.
-    setState(() {
-      Future<UserInfo> FutureProfileInfo = getUser();
-    });
+    futureProfileInfo = getUser();
+    futureGetBags = getBags();
   }
 
   Future<UserInfo> getUser() async {
@@ -42,12 +175,14 @@ class _Homepage extends State<Homepage>{
 
     if (_response.statusCode == 200) {
       var parsedJson = json.decode(_response.body);
-      SessionFirstName = parsedJson['firstname'];
-      SessionLastName = parsedJson['lastname'];
-      SessionEmail = parsedJson['email'];
-      SessionAge = parsedJson['detials']['age'];
-      SessionHeight = parsedJson['detials']['height'];
-      SessionWeight = parsedJson['detials']['weight'];
+      setState(() {
+        SessionFirstName = parsedJson['firstname'];
+        SessionLastName = parsedJson['lastname'];
+        SessionEmail = parsedJson['email'];
+        SessionAge = parsedJson['details']['age'];
+        SessionHeight = parsedJson['details']['height'];
+        SessionWeight = parsedJson['details']['weight'];
+      });
 
       return UserInfo.fromJson(json.decode(_response.body));
     }
@@ -56,49 +191,62 @@ class _Homepage extends State<Homepage>{
       print(_response.body);
     }
   }
+  Future<GetBags> getBags() async {
+    final http.Response _response = await http.get(
+      'http://localhost3000.us-east-2.elasticbeanstalk.com/api/golf/getAllGolfBags',
+      headers: headers,
+    );
 
-  static Text bagCount()
-  {
+    if (_response.statusCode == 200) {
+
+      return GetBags.fromJson(json.decode(_response.body));
+    }
+    else {
+      print(_response.statusCode);
+      print(_response.body);
+    }
+  }
+  Future<BagInfo> postBag(dynamic info) async {
+    final http.Response _response = await http.post(
+        'http://localhost3000.us-east-2.elasticbeanstalk.com/api/golf/createGolfBag',
+        headers: headers,
+        body: info
+    );
+
+    // If successful login, navigate to homepage.
+    if (_response.statusCode == 200) {
+      return BagInfo.fromJson(json.decode(_response.body));
+    }
+    // If it's some other error that the user doesn't need to know about.
+    else {
+      print(_response.statusCode);
+      print(_response.body);
+    }
+  }
+  Future<ClubInfo> postClub(dynamic info) async {
+    final http.Response _response = await http.post(
+        'http://localhost3000.us-east-2.elasticbeanstalk.com/api/golf/createGolfclub',
+        headers: headers,
+        body: info
+    );
+
+    // If successful login, navigate to homepage.
+    if (_response.statusCode == 200) {
+      return ClubInfo.fromJson(json.decode(_response.body));
+    }
+    // If it's some other error that the user doesn't need to know about.
+    else {
+      print(_response.statusCode);
+      print(_response.body);
+    }
+  }
+
+  Text bagCount() {
     if (_bags == 0)
       return Text("[No bags]");
     else
       return Text("[$_bags bags]");
   }
-
-  // These three are each of the tabs.
-  List<Widget> _widgetOptions = <Widget>[
-    Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text("Welcome to Sporta", style: largeStyle)
-        ]
-    ),
-    Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text("Personal", style: largeStyle),
-          Text("Golf Bags", style: largeStyle),
-          bagCount(),
-          Row(
-            children: <Widget>[
-              RaisedButton(
-                onPressed: (){
-                  _bags++;
-                  print("Added bag!");
-                },
-                child: Text("Add bag")
-              )
-            ],
-          )
-        ]
-    ),
-    Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget> [
-          Text("Events", style: largeStyle),
-        ]
-    )
-  ];
 
   // SetState for changing the tab index.
   void _onItemTapped(int index) {
@@ -107,6 +255,185 @@ class _Homepage extends State<Homepage>{
     });
   }
 
+  List<Widget> displayBags(){
+    BagList = new List<Widget>();
+
+    BagList.add(Text("Golf Bags", style: largeStyle));
+
+    if(_bags == 0) {
+        BagList.add(Text("No bags to show"));
+        BagList.add(bagCount());
+        BagList.add(Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+                onPressed: (){
+                  _simp();
+                },
+                child: Text("Add bag")
+            )
+          ],
+        ));
+        return BagList;
+    }
+    else {
+      BagList.add(bagCount());
+      BagList.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+              onPressed: (){
+                _simp();
+              },
+              child: Text("Add bag")
+          )
+        ],
+      ));
+      return BagList;
+    }
+  }
+
+  String determineClubType() {
+    if(_numClubs == 0)
+      return NO_CLUB;
+
+    switch(_clubType)
+    {
+      case "Wood":
+        return WOOD;
+
+      case "Iron":
+        return IRON;
+
+      case "Wedge":
+        return WEDGE;
+
+      case "Putter":
+        return PUTTER;
+    }
+  }
+
+  Widget select(){
+
+    switch (_selectedIndex)
+    {
+      case 0: return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text("Welcome to Sporta", style: largeStyle)
+          ]
+      );
+      break;
+
+      case 1: return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: displayBags()
+      );
+      break;
+
+      case 2: return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget> [
+            Text("Events", style: largeStyle),
+          ]
+      );
+    }
+  }
+
+  _simp() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Bag', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  TextField(
+                      controller: _bagNameController,
+                      decoration: InputDecoration(hintText: "Bag Name")
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 35, 0, 0),
+                    child: Text(
+                        "Club", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  ),
+                  TextField(
+                      controller: _clubNameController,
+                      decoration: InputDecoration(hintText: "Club Name")
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Text("Club type"),
+                  ),
+                  DropdownButton<String>(
+                    value: _clubType,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color:Colors.deepOrange),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        _clubType = newValue;
+                      });
+                      },
+                    items: <String> ['Wood', 'Iron', 'Wedge', 'Putter'].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value, style: TextStyle(fontSize: 18)),
+                      );
+                    }).toList(),
+                  ),
+                  RaisedButton(
+                    child: Text("Add club"),
+                    onPressed: (){
+                      if (_clubNameController.text == "")
+                        DialogPopUp(context, "Name your club.");
+                      setState(() {
+                        _numClubs++;
+                      });
+                    },
+                  ),
+                  Text(_numClubs == 1 ? "$_numClubs club will be in this bag"
+                      : "$_numClubs clubs will be in this bag", style: TextStyle(fontSize: 18))
+                ],
+              )
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                  child: new Text('Add bag'),
+                  onPressed: () {
+                    if (_bagNameController.text == "")
+                      DialogPopUp(context, "Name your bag.");
+                    else {
+                      futureBagInfo = postBag(jsonEncode(
+                          <String, String>{'bagName': _bagNameController.text}));
+                      futureGetBags = getBags();
+                      /*futureClubInfo = postClub(
+                          jsonEncode(<String, String>{
+                            'golfBag': bagID,
+                            'clubName': _clubNameController.text,
+                            'clubType': determineClubType(),
+                          })
+                      );*/
+                      _bags++;
+                      Navigator.of(context).pop();
+                    }
+                  }
+              ),
+              new FlatButton(
+                child: new Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _bagNameController.text = "";
+                  _clubNameController.text = "";
+                  _numClubs = 0;
+                }
+              )
+            ],
+          );
+        });
+  }
   // Displays the floating action button if we're on the events page.
   FloatingActionButton isIndex2() {
     // If the current index is 2 (events page) return the button.
@@ -168,11 +495,10 @@ class _Homepage extends State<Homepage>{
                 )
             ),
           body: Center(
-            child: _widgetOptions.elementAt(_selectedIndex),
-
+            child: select(),
           ),
           bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
+            items: <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                 icon: Icon(Icons.home),
                 title: Text('Overview'),
@@ -191,7 +517,7 @@ class _Homepage extends State<Homepage>{
             onTap: _onItemTapped,
           ),
           floatingActionButton: isIndex2(),
-      )
+      ),
     );
   }
 }
