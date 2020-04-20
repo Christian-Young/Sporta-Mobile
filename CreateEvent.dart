e99946b2ee2bc28ef810264e297ee1b419c34a49
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-04-19 21:57:13 -0400

    100% done

--- CreateEvent.dart
+++ CreateEvent.dart
@@ -6,20 +6,22 @@ import 'Session.dart';
 import 'dart:convert';
 import 'dart:async';
 
-
 class CreateEvent extends StatefulWidget{
   _CreateEvent createState() => _CreateEvent();
 }
 
 class _CreateEvent extends State<CreateEvent>{
-  final _titleController = TextEditingController();
+  final _eventNameController = TextEditingController();
   final _courseController = TextEditingController();
   final _holeController = TextEditingController();
   List<TextEditingController> _parsController;
   List<Widget> _holeList;
   List<Widget> _playerList;
   List<TextEditingController> _controllerList;
-  String _numPlayers = '1';
+  String _numPlayers = '2';
+  DateTime selectedStartDate = DateTime.now();
+  DateTime selectedEndDate = DateTime.now();
+  List<String> listPlayers = new List<String>();
 
   Future<EventInfo> event(dynamic info) async {
     final http.Response _response = await http.post(
@@ -29,6 +31,7 @@ class _CreateEvent extends State<CreateEvent>{
     );
 
     if (_response.statusCode == 200) {
+      Navigator.pop(context);
       //return EventInfo.fromJson(json.decode(_response.body));
     }
     else {
@@ -37,6 +40,7 @@ class _CreateEvent extends State<CreateEvent>{
     }
   }
 
+/*
   void createHoles(int holeCount){
     _parsController = new List<TextEditingController>(holeCount);
     _holeList = new List<Widget>();
@@ -68,7 +72,7 @@ class _CreateEvent extends State<CreateEvent>{
     if(_holeController.text != '')
     {
       if (int.parse(_holeController.text) > 0)
-        return new Text("Pars:", textAlign: TextAlign.center, style: TextStyle(fontSize: 17));
+        return new Text("Pars:", textAlign: TextAlign.center, style: TextStyle(fontSize: 20));
       else
         return new Text("");
     }
@@ -86,7 +90,7 @@ class _CreateEvent extends State<CreateEvent>{
     }
     else
       return new Row();
-  }
+  }*/
 
   Row displayPlayers(int playerCount){
     _controllerList = new List<TextEditingController>();
@@ -99,12 +103,35 @@ class _CreateEvent extends State<CreateEvent>{
           Expanded(child: TextField(
             controller: _controllerList[i],
               textAlign: TextAlign.center,
-              decoration: InputDecoration(hintText: "Player Email")
+              decoration: InputDecoration(hintText: "Email")
           )
         )
       );
     }
     return Row(children: _playerList);
+  }
+
+  Future<void> _selectStartDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedStartDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedStartDate)
+      setState(() {
+        selectedStartDate = picked;
+      });
+  }
+  Future<void> _selectEndDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedEndDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedEndDate)
+      setState(() {
+        selectedEndDate = picked;
+      });
   }
 
   Widget build(BuildContext context) {
@@ -119,10 +146,10 @@ class _CreateEvent extends State<CreateEvent>{
             Padding(
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
                 child: TextField(
-                    controller: _titleController,
+                    controller: _eventNameController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'Title',
+                      labelText: 'Event Name',
                     )
                 )
             ),
@@ -136,7 +163,31 @@ class _CreateEvent extends State<CreateEvent>{
                     )
                 )
             ),
-            Container(
+            Text("${selectedStartDate.toLocal()}".split(' ')[0],
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Align(
+              child: RaisedButton(
+                onPressed: (){
+                  _selectStartDate(context);
+                },
+                child: Text("Select Start Date"),
+              ),
+            ),
+            Text("${selectedEndDate.toLocal()}".split(' ')[0],
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Align(
+              child: RaisedButton(
+                onPressed: (){
+                  _selectEndDate(context);
+                },
+                child: Text("Select End Date"),
+              ),
+            ),
+            /*Container(
               width: 70.0,
               child: TextField(
                 style: TextStyle(
@@ -154,9 +205,15 @@ class _CreateEvent extends State<CreateEvent>{
                     labelText: 'Holes',
                   )
               ),
+            ),*/
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
             ),
-            areParsDisplayed(),
-            parsForEachHole(),
+            //areParsDisplayed(),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+            ),
+            //parsForEachHole(),
             Padding(
                 padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
               child: Text("Players:", textAlign: TextAlign.center ,style: TextStyle(fontSize: 20)),
@@ -174,7 +231,7 @@ class _CreateEvent extends State<CreateEvent>{
                     _numPlayers = newValue;
                   });
                 },
-                items: <String> ['1', '2', '3', '4'].map<DropdownMenuItem<String>>((String value) {
+                items: <String> ['2', '3', '4', '5'].map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
                     child: Text(value, style: TextStyle(fontSize: 18)),
@@ -185,7 +242,15 @@ class _CreateEvent extends State<CreateEvent>{
             Align(
               child: RaisedButton(
                   onPressed: (){
-                    Navigator.pop(context);
+                    for (int i = 0; i < _controllerList.length; i++)
+                      listPlayers.add(_controllerList[i].text);
+                    event(jsonEncode({
+                      'eventName': _eventNameController.text,
+                      'startDate': selectedStartDate.toIso8601String() + 'Z',
+                      'endDate': selectedEndDate.toIso8601String() + 'Z',
+                      'course' : _courseController.text,
+                      'players' : listPlayers
+                    }));
                   },
                   child: Text('Create Event')
               ),
